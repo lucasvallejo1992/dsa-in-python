7c46ad056587efcce9d1eaf179c9022343113b08
Lucas Vallejo <97030519+lucasvallejo1992@users.noreply.github.com>
2024-03-21 22:39:21 +0100

    feat: singly linked list insert after node

--- linked-list/singly-linked-list.py
+++ linked-list/singly-linked-list.py
@@ -11,10 +11,7 @@ class LinkedList:
     cur_node = self.head
     print_value = ""
     while cur_node:
-      if cur_node.next is None:
-        print_value += str(cur_node.data)
-      else:
-        print_value += str(cur_node.data) + " -> "
+      print_value += str(cur_node.data) + ("" if cur_node.next == None else " -> ")
       cur_node = cur_node.next
     print(print_value)
 
@@ -29,6 +26,24 @@ class LinkedList:
     while last_node.next:
       last_node = last_node.next
     last_node.next = new_node
+  
+  def prepend(self, data):
+    new_node = Node(data)
+
+    new_node.next = self.head
+    self.head = new_node
+
+  def insert_after_node(self, prev_node, data):
+    if not prev_node:
+      print("Previous node does not exist.")
+      return
+    if not isinstance(prev_node, Node):
+      print("Previous node is not a Node object.")
+      return
+
+    new_node = Node(data)
+    new_node.next = prev_node.next
+    prev_node.next = new_node
 
 
 # Test
@@ -37,6 +52,10 @@ linked_list.append("A")
 linked_list.append("B")
 linked_list.append("C")
 linked_list.append("D")
+
+linked_list.insert_after_node(linked_list.head.next, "Y")
+
+linked_list.prepend("Z")
 
 
 linked_list.print_list() 
